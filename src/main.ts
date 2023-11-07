7c01abba4f2aa07dbf7763100c217e53ada1bb7b
badhon <mylaptop512@gmail.com>
2023-11-07 23:40:46 +0600

    working

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+interface Todo {
+    title:string,
+    isCompleted:boolean,
+    readonly id:string
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const todos:Todo[]=[]
+
+const todosContainer=document.querySelector(".todosContainer") as HTMLDivElement
+const todoInput=document.getElementsByName("input")[0] as HTMLInputElement
+const form= document.getElementById("#form") as HTMLFormElement
